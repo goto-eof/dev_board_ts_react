6d512b8cfb38b8a46d18dac965ed68e36de5fba6
Andrei Dodu <andrei.dodu@hotmail.it>
2022-12-10 20:02:28 +0100

    chore: configuration update

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -8,7 +8,7 @@ import {
   Heading,
   Button,
 } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import GenericService from '../service/GenerciService';
 import Result from '../core/ResultI';
@@ -22,8 +22,8 @@ export default function LoginForm({
   toggleChangedLocalStorage,
 }: LoginFormProps) {
   const [states, setStates] = useState({
-    username: 'admin',
-    password: 'password',
+    username: '',
+    password: '',
     error: new Map<string, boolean>(),
     isInvalid: false,
     columns: Array<ColumnResponseI>(),
